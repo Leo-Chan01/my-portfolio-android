a312d7e09f046a462ca3cfe0b6fd411e830fb963
Leo-Chan01 <ephraimleo16@gmail.com>
2022-04-30 22:36:59 +0100

    Initial Commit:
    - Initial setup of the environment for code

---
+++ app/src/main/java/com/raym/umunnakweephraim/ui/me/MeFragment.java
@@ -0,0 +1,37 @@
+package com.raym.umunnakweephraim.ui.me;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
+
+import com.raym.umunnakweephraim.databinding.FragmentMeBinding;
+
+public class MeFragment extends Fragment {
+
+    private FragmentMeBinding binding;
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+        DashboardViewModel dashboardViewModel =
+                new ViewModelProvider(this).get(DashboardViewModel.class);
+
+        binding = FragmentMeBinding.inflate(inflater, container, false);
+        View root = binding.getRoot();
+
+        final TextView textView = binding.textDashboard;
+        dashboardViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+        return root;
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
+    }
+}
